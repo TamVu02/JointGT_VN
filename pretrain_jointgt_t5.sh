5b3867c133ea0d9559f83e787c17876dfa74da1f
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2023-12-21 17:19:25 +0700

    Update pretrain_jointgt_t5.sh

--- pretrain_jointgt_t5.sh
+++ pretrain_jointgt_t5.sh
@@ -7,12 +7,12 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python /content/JointGT_VN/cli_gt.py \
         --model_path VietAI/vit5-base \
         --tokenizer_path VietAI/vit5-base \
         --output_dir pretrain_model/jointgt_t5 \
-        --train_file /content/train_data \
-        --predict_file /content/test_data \
-        --knowledge_file /content/knowledge-full \
+        --train_file /content/drive/MyDrive/jointgt_data/w_train \
+        --predict_file /content/drive/MyDrive/jointgt_data/w_test \
+        --knowledge_file /content/drive/MyDrive/jointgt_data/knowledge-full \
         --train_batch_size 32 \
         --predict_batch_size 32 \
-        --max_input_length 600 \
+        --max_input_length 2000 \
         --max_output_length 64 \
         --gradient_accumulation_steps 8 \
         --learning_rate 3e-5 \
